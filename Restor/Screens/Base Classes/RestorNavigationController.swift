a8ce590491beb111f93bae9d56832f2092fb3f25
Jaseem V V <jaseemvv@icloud.com>
2023-10-29 03:43:12 +0530

    Clean up and base class should popup method

--- Restor/Screens/Base Classes/RestorNavigationController.swift
+++ Restor/Screens/Base Classes/RestorNavigationController.swift
@@ -12,7 +12,6 @@ import UIKit
 /// Ability to intercept back button tap to determine if the page should be changed.
 class RestorNavigationController: UINavigationController {
     weak var navDelegate: RestorTableViewController?
-    var shouldPopOnBackButton: (() -> Bool)?
 
     override func viewDidLoad() {
         super.viewDidLoad()
