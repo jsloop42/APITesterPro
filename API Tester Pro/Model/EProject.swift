ab6bf2088587833564302aae5d620bd9e59e9c74
Jaseem V V <jaseemvv@icloud.com>
2023-11-03 02:15:36 +0530

    Rename Restor usages to API Tester Pro

---
+++ API Tester Pro/Model/EProject.swift
@@ -0,0 +1,197 @@
+//
+//  EProject.swift
+//  API Tester Pro
+//
+//  Created by jsloop on 22/03/20.
+//  Copyright © 2020 EstoApps OÜ. All rights reserved.
+//
+
+import Foundation
+import CloudKit
+import CoreData
+
+public class EProject: NSManagedObject, Entity {
+    public var recordType: String { return "Project" }
+    
+    public func getId() -> String {
+        return self.id ?? ""
+    }
+    
+    public func getWsId() -> String {
+        return self.wsId ?? ""
+    }
+    
+    public func setWsId(_ id: String) {
+        self.wsId = id
+    }
+    
+    public func getName() -> String {
+        return self.name ?? ""
+    }
+    
+    public func getCreated() -> Int64 {
+        return self.created
+    }
+    
+    public func getModified() -> Int64 {
+        return self.modified
+    }
+    
+    public func setModified(_ ts: Int64? = nil) {
+        self.modified = ts ?? Date().currentTimeNanos()
+    }
+    
+    public func getChangeTag() -> Int64 {
+        return self.changeTag
+    }
+    
+    public func setChangeTag(_ ts: Int64? = nil) {
+        self.changeTag = ts ?? Date().currentTimeNanos()
+    }
+    
+    public func getVersion() -> Int64 {
+        return self.version
+    }
+    
+    public func setIsSynced(_ status: Bool) {
+        self.isSynced = status
+    }
+    
+    public func setMarkedForDelete(_ status: Bool) {
+        self.markForDelete = status
+    }
+    
+    public override func willSave() {
+        //if self.modified < AppState.editRequestSaveTs { self.modified = AppState.editRequestSaveTs }
+    }
+    
+    func updateCKRecord(_ record: CKRecord, workspace: CKRecord) {
+        self.managedObjectContext?.performAndWait {
+            record["created"] = self.created as CKRecordValue
+            record["modified"] = self.modified as CKRecordValue
+            record["changeTag"] = self.changeTag as CKRecordValue
+            record["desc"] = (self.desc ?? "") as CKRecordValue
+            record["id"] = self.getId() as CKRecordValue
+            record["wsId"] = self.getWsId() as CKRecordValue
+            record["name"] = (self.name ?? "") as CKRecordValue
+            record["version"] = self.version as CKRecordValue
+            let ref = CKRecord.Reference(record: workspace, action: .none)
+            record["workspace"] = ref
+        }
+    }
+    
+    static func addRequestReference(to project: CKRecord, request: CKRecord) {
+        let ref = CKRecord.Reference(record: request, action: .deleteSelf)
+        var xs = project["requests"] as? [CKRecord.Reference] ?? [CKRecord.Reference]()
+        if !xs.contains(ref) {
+            xs.append(ref)
+            project["requests"] = xs as CKRecordValue
+        }
+    }
+    
+    static func addRequestMethodReference(to project: CKRecord, requestMethod: CKRecord) {
+        let ref = CKRecord.Reference(record: requestMethod, action: .deleteSelf)
+        var xs = project["requestMethods"] as? [CKRecord.Reference] ?? [CKRecord.Reference]()
+        if !xs.contains(ref) {
+            xs.append(ref)
+            project["requestMethods"] = xs as CKRecordValue
+        }
+    }
+    
+    static func getWorkspace(_ record: CKRecord, ctx: NSManagedObjectContext) -> EWorkspace? {
+        if let ref = record["workspace"] as? CKRecord.Reference {
+            return CoreDataService.shared.getWorkspace(id: EACloudKit.shared.entityID(recordID: ref.recordID), ctx: ctx)
+        }
+        return nil
+    }
+    
+    /// Returns project from the given record reference. If the project does not exists, one will be created.
+    static func getProjectFromReference(_ ref: CKRecord.Reference, record: CKRecord, ctx: NSManagedObjectContext) -> EProject? {
+        let projId = EACloudKit.shared.entityID(recordID: ref.recordID)
+        let wsId = record.getWsId()
+        if let proj = CoreDataService.shared.getProject(id: projId, ctx: ctx) { return proj }
+        let proj = CoreDataService.shared.createProject(id: projId, wsId: wsId, name: "", desc: "", checkExists: false, ctx: ctx)
+        proj?.changeTag = 0
+        return proj
+    }
+    
+    static func getRequestRecordIDs(_ record: CKRecord) -> [CKRecord.ID] {
+        if let xs = record["requests"] as? [CKRecord.Reference] {
+            return xs.map { ref -> CKRecord.ID in ref.recordID }
+        }
+        return []
+    }
+    
+    func updateFromCKRecord(_ record: CKRecord, ctx: NSManagedObjectContext) {
+        if let moc = self.managedObjectContext {
+            moc.performAndWait {
+                if let x = record["created"] as? Int64 { self.created = x }
+                if let x = record["modified"] as? Int64 { self.modified = x }
+                if let x = record["changeTag"] as? Int64 { self.changeTag = x }
+                if let x = record["desc"] as? String { self.desc = x }
+                if let x = record["id"] as? String { self.id = x }
+                if let x = record["wsId"] as? String { self.wsId = x }
+                if let x = record["name"] as? String { self.name = x }
+                if let x = record["version"] as? Int64 { self.version = x }
+                if let ws = EProject.getWorkspace(record, ctx: moc) { self.workspace = ws }
+            }
+        }
+    }
+    
+    public static func fromDictionary(_ dict: [String: Any]) -> EProject? {
+        guard let id = dict["id"] as? String, let wsId = dict["wsId"] as? String else { return nil }
+        let db = CoreDataService.shared
+        guard let proj = db.createProject(id: id, wsId: wsId, name: "", desc: "", ctx: db.mainMOC) else { return nil }
+        if let x = dict["created"] as? Int64 { proj.created = x }
+        if let x = dict["modified"] as? Int64 { proj.modified = x }
+        if let x = dict["changeTag"] as? Int64 { proj.changeTag = x }
+        if let x = dict["desc"] as? String { proj.desc = x }
+        if let x = dict["name"] as? String { proj.name = x }
+        if let x = dict["version"] as? Int64 { proj.version = x }
+        if let xs = dict["requests"] as? [[String: Any]] {
+            xs.forEach { dict in
+                if let req = ERequest.fromDictionary(dict) {
+                    req.project = proj
+                }
+            }
+        }
+        if let xs = dict["methods"] as? [[String: Any]] {
+            xs.forEach { dict in
+                if let method = ERequestMethodData.fromDictionary(dict) {
+                    method.project = proj
+                }
+            }
+        }
+        proj.markForDelete = false
+        db.saveMainContext()
+        return proj
+    }
+    
+    public func toDictionary() -> [String: Any] {
+        var dict: [String: Any] = [:]
+        dict["created"] = self.created
+        dict["modified"] = self.modified
+        dict["changeTag"] = self.changeTag
+        dict["desc"] = self.desc
+        dict["id"] = self.id
+        dict["wsId"] = self.wsId
+        dict["name"] = self.name
+        dict["version"] = self.version
+        var xs: [[String: Any]] = []
+        let db = CoreDataService.shared
+        // requests
+        let reqs = db.getRequests(projectId: self.getId())
+        reqs.forEach { req in
+            xs.append(req.toDictionary())
+        }
+        dict["requests"] = xs
+        xs = []
+        // request methods
+        let reqMethods = db.getRequestMethodData(projId: self.getId())
+        reqMethods.forEach { method in
+            xs.append(method.toDictionary())
+        }
+        dict["methods"] = xs
+        return dict
+    }
+}
---
+++ API Tester Pro/Model/SecureTransformer.swift
@@ -0,0 +1,104 @@
+//
+//  SecureTransformer.swift
+//  API Tester Pro
+//
+//  Created by jsloop on 15/06/20.
+//  Copyright © 2020 EstoApps OÜ. All rights reserved.
+//
+
+import Foundation
+
+fileprivate struct SecureTransformerInfo {
+    private static let utils = { EAUtils.shared }()
+    private static let _key: [UInt8] = [79, 87, 53, 108, 90, 72, 73, 53, 78, 87, 49, 108, 100, 87, 120, 106, 97, 68, 74, 109, 99, 88, 86, 51, 90, 68, 70, 105,
+                                        79, 84, 104, 120, 100, 110, 90, 104, 99, 71, 49, 122, 97, 51, 77, 61]
+    private static let _iv: [UInt8] = [78, 84, 82, 120, 90, 106, 70, 108, 100, 84, 70, 104, 100, 87, 69, 52, 97, 109, 100, 51, 101, 65, 61, 61]
+    
+    static var key: String {
+        guard let str = String(data: Data(_key), encoding: .utf8) else { return "" }
+        return self.utils.base64Decode(str) ?? ""
+    }
+    static var iv: String {
+        guard let str = String(data: Data(_iv), encoding: .utf8) else { return "" }
+        return self.utils.base64Decode(str) ?? ""
+    }
+}
+
+/// A class that can be used for encrypting and decrypting core data `String` values on the fly.
+class SecureTransformerString: NSSecureUnarchiveFromDataTransformer {
+    private let aes = AES(key: SecureTransformerInfo.key, iv: SecureTransformerInfo.iv)
+    
+    override class var allowedTopLevelClasses: [AnyClass] {
+        return [NSString.self]
+    }
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSString.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+    
+    static let name = NSValueTransformerName(rawValue: String(describing: SecureTransformerString.self))
+    
+    public static func register() {
+        let transformer = SecureTransformerString()
+        ValueTransformer.setValueTransformer(transformer, forName: name)
+    }
+
+    /// Encrypt the value.
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let text = value as? String else { return nil }
+        return self.aes?.encrypt(string: text)
+    }
+
+    /// Decrypt the value.
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else { return nil }
+        if let res = self.aes?.decrypt(data: data) {
+            return String(data: res, encoding: .utf8)
+        }
+        return nil
+    }
+}
+
+/// A class that can be used for encrypting and decrypting core data `Data` values on the fly.
+class SecureTransformerData: NSSecureUnarchiveFromDataTransformer {
+    private let aes = AES(key: SecureTransformerInfo.key, iv: SecureTransformerInfo.iv)
+    
+    override class var allowedTopLevelClasses: [AnyClass] {
+        return [NSData.self]
+    }
+    
+    override class func transformedValueClass() -> AnyClass {
+        return NSData.self
+    }
+    
+    override class func allowsReverseTransformation() -> Bool {
+        return true
+    }
+    
+    static let name = NSValueTransformerName(rawValue: String(describing: SecureTransformerData.self))
+    
+    public static func register() {
+        let transformer = SecureTransformerData()
+        ValueTransformer.setValueTransformer(transformer, forName: name)
+    }
+
+    /// Decrypt the value.
+    /// - Parameter value: A data value
+    /// - Returns: Encrypted binary data
+    override func transformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else { return nil }
+        return self.aes?.encrypt(data: data)
+    }
+
+    /// Decrypt the value.
+    /// - Parameter value: A transformed data value
+    /// - Returns: Decrypted binary data
+    override func reverseTransformedValue(_ value: Any?) -> Any? {
+        guard let data = value as? Data else { return nil }
+        return self.aes?.decrypt(data: data)  // returns Data
+    }
+}
