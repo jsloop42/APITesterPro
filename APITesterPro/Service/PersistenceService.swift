09ac31fa95f2d168756b3d8ba73e94d70f9e90ac
Jaseem V V <jaseemvv@icloud.com>
2023-12-02 16:25:00 +0530

    Delete project from Core Data
    - Refactor markForDelete method to use named argument

--- APITesterPro/Service/PersistenceService.swift
+++ APITesterPro/Service/PersistenceService.swift
@@ -16,64 +16,64 @@ class PersistenceService {
     // TODO: currently entity is directly deleted. With CloudKit sync, first mark it a deleted, save context and add it to cloudkit delete queue. And on cloudkit delete, delete from core data.
     
     /// Delete workspace
-    func markEntityForDelete(_ ws: EWorkspace) {
+    func markEntityForDelete(ws: EWorkspace) {
         self.db.deleteEntity(ws)
         self.db.saveMainContext()
     }
     
     /// Delete environment
-    func markEntityForDelete(_ env: EEnv) {
+    func markEntityForDelete(env: EEnv) {
         self.db.deleteEntity(env)
         self.db.saveMainContext()
     }
     
     
     /// Delete environment variable
-    func markEntityForDelete(_ envVar: EEnvVar) {
+    func markEntityForDelete(envVar: EEnvVar) {
         self.db.deleteEntity(envVar)
         self.db.saveMainContext()
     }
     
     /// Delete project
-    func markEntityForDelete(_ proj: EProject) {
+    func markEntityForDelete(proj: EProject) {
         self.db.deleteEntity(proj)
         self.db.saveMainContext()
     }
     
     /// Delete request method data
-    func markEntityForDelete(_ reqMethodData: ERequestMethodData) {
+    func markEntityForDelete(reqMethodData: ERequestMethodData) {
         self.db.deleteEntity(reqMethodData)
         self.db.saveMainContext()
     }
     
     /// Delete request
-    func markEntityForDelete(_ req: ERequest) {
+    func markEntityForDelete(req: ERequest) {
         self.db.deleteEntity(req)
         self.db.saveMainContext()
     }
     
     /// Delete request data
-    func markEntityForDelete(_ reqData: ERequestData) {
+    func markEntityForDelete(reqData: ERequestData) {
         self.db.deleteEntity(reqData)
         self.db.saveMainContext()
     }
     
-    func markEntityForDelete(_ reqBodyData: ERequestBodyData) {
+    func markEntityForDelete(reqBodyData: ERequestBodyData) {
         self.db.deleteEntity(reqBodyData)
         self.db.saveMainContext()
     }
     
-    func markEntityForDelete(_ file: EFile) {
+    func markEntityForDelete(file: EFile) {
         self.db.deleteEntity(file)
         self.db.saveMainContext()
     }
     
-    func markEntityForDelete(_ image: EImage) {
+    func markEntityForDelete(image: EImage) {
         self.db.deleteEntity(image)
         self.db.saveMainContext()
     }
     
-    func markEntityForDelete(_ history: EHistory) {
+    func markEntityForDelete(history: EHistory) {
         self.db.deleteEntity(history)
         self.db.saveMainContext()
     }
