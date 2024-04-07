ab85dd37d49ea2cde352a01951216a62aa98a796
Jaseem V V <jaseemvv@icloud.com>
2024-04-07 20:05:18 +0530

    Add logger macros for Objective-C

---
+++ APITesterPro/EALib/Utils.h
@@ -0,0 +1,28 @@
+//
+//  Utils.h
+//  APITesterPro
+//
+//  Created by jsloop on 07.04.2024.
+//  Copyright © 2024 Jaseem V V. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+#define NSLog(FORMAT, ...) fprintf(stderr, "%s\n", [[NSString stringWithFormat:FORMAT, ##__VA_ARGS__] UTF8String]);
+
+#ifdef DEBUG
+#   define debug(...) NSLog(__VA_ARGS__)
+#   define info(...) NSLog(__VA_ARGS__)
+#   define error(...) NSLog(__VA_ARGS__)
+#else
+#   define info(...) NSLog(__VA_ARGS__)
+#   define error(...) NSLog(__VA_ARGS__)
+#endif
+
+@interface Utils : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
